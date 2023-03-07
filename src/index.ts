e4f472d1d8fca790f3b13408df218e4bc1571485
Abdullah-Alsalhi <abuhamed19@gmail.com>
2023-03-07 20:06:35 +0300

    set up the project

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
