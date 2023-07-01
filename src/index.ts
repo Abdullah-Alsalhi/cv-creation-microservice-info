e350de9b908e6fdaaef7b6915fc311fe3a8e6d8d
Abdullah-Alsalhi <abuhamed19@gmail.com>
2023-07-01 20:31:59 +0300

    added 404 route

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,10 @@ app.use(
 	skill_info_route,
 	project_info_route
 );
+// 404 Not Found route
+app.use((req, res, next) => {
+	res.status(404).json({ error: "Not Found" });
+});
 
 // Todo: check the connection to the db before starting the app
 // CONNECTION AND START THE APP
