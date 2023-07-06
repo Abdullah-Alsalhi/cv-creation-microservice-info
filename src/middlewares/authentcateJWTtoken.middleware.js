94448d4aff91f53d203ad229da4aa806a21781ee
Abdullah-Alsalhi <abuhamed19@gmail.com>
2023-07-06 20:32:22 +0300

    untracked  files

--- src/middlewares/authentcateJWTtoken.middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.authenticateJWTtoken = void 0;
-var passport_1 = require("passport");
-exports.authenticateJWTtoken = passport_1["default"].authenticate("jwt", {
-    session: false
-});
